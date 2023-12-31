46dcf672f588a85f56f272aff2eaed23766cb142
Nate Tilley <nathan.tilley@areaone.cloud>
2023-12-31 14:08:18 +0000

    logging

--- anonymousrequests/src/main/java/cloud/areaone/spotify/ArtistConcertsRequest.java
+++ anonymousrequests/src/main/java/cloud/areaone/spotify/ArtistConcertsRequest.java
@@ -9,6 +9,8 @@ import org.apache.hc.core5.http.io.HttpClientResponseHandler;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.message.BasicHeader;
 import org.apache.hc.core5.net.URIBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
@@ -19,6 +21,7 @@ import static java.net.HttpURLConnection.HTTP_OK;
 
 public class ArtistConcertsRequest
 {
+    final static Logger logger = LoggerFactory.getLogger(ArtistConcertsRequest.class);
     public static final String CONCERT_URL = "https://spclient.wg.spotify.com/concerts/v2/concerts/artist/";
 
     private final HttpClientResponseHandler<String> responseHandler = response -> {
@@ -44,10 +47,10 @@ public class ArtistConcertsRequest
             BasicHeader authorizationHeader = new BasicHeader("Authorization",
                                                               "Bearer " + credentials.getAccessToken());
             request.addHeader(authorizationHeader);
+            logger.debug("request: " + request);
 
             String response = httpClient.execute(request, responseHandler);
-
-            System.out.println("response: " + response);
+            logger.debug("response: " + response);
 
             return new Gson().fromJson(response, ArtistConcertResponse.class);
         }
